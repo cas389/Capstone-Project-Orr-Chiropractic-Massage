2fe30d5cffa1ef9cc9a0999c1302e8fd4cb09025
cas389 <Cheryl.Ju@ufl.edu>
2023-03-09 20:47:54 -0500

    worked on navigation

--- wp-content/themes/capstone-theme/header.php
+++ wp-content/themes/capstone-theme/header.php
@@ -14,37 +14,39 @@
 
 
   <body <?php body_class() ?>>
-    <header class="container">
-      <div class="row">
-        <div class="logo col-md-3">
-          <?php
-            // Display logo if it's set, if not display site title
-            if(get_header_image() == ''){ ?>
-              <h1 class="header-logo-text"><a href="<?php echo get_home_url(); ?>"><?php bloginfo('name'); ?></a></h1>
+    <header>
+      <div class="container">
+        <div class="row">
+          <div class="logo col-md-3">
             <?php
-            }else { ?>
-              <a href="<?php echo get_home_url(); ?>"><img class="logo" src="<?php header_image(); ?>" height="<?php echo get_custom_header()->height; ?>" width="<?php echo get_custom_header()->width; ?>" alt="Company Logo" /></a>
-          <?php
-            }
-          ?>
-        </div>
-
-        <div class="col-md-9">
-          <!-- Hamburger Icon for Mobile -->
-          <input class="side-menu" type="checkbox" id="side-menu"/>
-          <label class="hamburger-menu" for="side-menu"><span class="hamb-line"></span></label>
-
-
-          <nav class="main-navigation">
-              <!-- Navigation Bar -->
+              // Display logo if it's set, if not display site title
+              if(get_header_image() == ''){ ?>
+                <h1 class="header-logo-text"><a href="<?php echo get_home_url(); ?>"><?php bloginfo('name'); ?></a></h1>
               <?php
-                // Shows the navigation to the page, created by the user
-                wp_nav_menu(array(
-                  'theme_location'  => 'main-menu',
-                ));
-              ?>
+              }else { ?>
+                <a href="<?php echo get_home_url(); ?>"><img class="logo" src="<?php header_image(); ?>" height="<?php echo get_custom_header()->height; ?>" width="<?php echo get_custom_header()->width; ?>" alt="Company Logo" /></a>
+            <?php
+              }
+            ?>
+          </div>
 
-          </nav>
+          <div class="col-md-9">
+            <!-- Hamburger Icon for Mobile -->
+            <input class="side-menu" type="checkbox" id="side-menu"/>
+            <label class="hamburger-menu" for="side-menu"><span class="hamb-line"></span></label>
+
+
+            <nav class="main-navigation">
+                <!-- Navigation Bar -->
+                <?php
+                  // Shows the navigation to the page, created by the user
+                  wp_nav_menu(array(
+                    'theme_location'  => 'main-menu',
+                  ));
+                ?>
+
+            </nav>
+          </div>
         </div>
       </div>
     </header>
