5a613f27b18a2514588d47e340a375ebe851e933
Maddie Pesce <70730051+maddiepesce@users.noreply.github.com>
2023-04-12 15:30:13 -0400

    Create thank-you.php
    Thank you page for contact form php

---
+++ thank-you.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+    <head>
+    <meta charset="utf-8">
+        <title>Contact Form</title>
+
+        <link rel="stylesheet" type="text/css" href="css/style.css" media="all">
+
+        <!-- GOOGLE FONTS -->
+        <link rel="stylesheet" href="https://use.typekit.net/qcf2avp.css">
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=PT+Serif:ital,wght@0,400;0,700;1,400;1,700&display=swap" rel="stylesheet">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css"/>
+
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+
+<body class="thank-you-body">
+    <div class="container-c">
+    <div class="content">
+
+<p>Thank you for your submission. A member of our team will be back to you shortly.</p>
+
+</div>
+</div>
+
+</body>
+</html>
